362f77702b95c69e4aca1cca804909d6691247d6
khadziabdi1 <khadziabdi1@etf.unsa.ba>
2024-02-06 12:33:57 +0100

    Method Sum of wells modification

--- tetrisSimulation.py
+++ tetrisSimulation.py
@@ -130,16 +130,22 @@ class TetrisSimulation:
                 well_depths.append(well_depth)
             else:
                 well_depths.append(0)
-                
+
         return max(well_depths, default=0)
 
     def sum_of_wells(self, board):
         well_depths = []
-        for col in range(board.shape[1] - 1):
-            occurrences_col = np.where(board[:, col] == 1)[0]
-            occurrences_col_next = np.where(board[:, col + 1] == 1)[0]
-            if occurrences_col.size > 0 and occurrences_col_next.size > 0:
-                well_depths.append(max(0, np.min(occurrences_col) - np.min(occurrences_col_next)))
+
+        for col in range(board.shape[1]):
+            left_side = board[:, :col] if col > 0 else np.ones_like(board[:, :1])
+            right_side = board[:, col+1:] if col < board.shape[1] - 1 else np.ones_like(board[:, -1:])
+
+            left_occupied_row = np.min(np.where(left_side == 1)[0]) if np.any(left_side == 1) else board.shape[0]
+            right_occupied_row = np.min(np.where(right_side == 1)[0]) if np.any(right_side == 1) else board.shape[0]
+
+            well_depth = min(np.where(board[:,col]==1)[0])-max(left_occupied_row, right_occupied_row)
+            if(well_depth > 0):
+                well_depths.append(well_depth)
             else:
                 well_depths.append(0)
         return sum(well_depths)
